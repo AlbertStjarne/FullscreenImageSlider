8df4ce72aec73a979a2c9397a249392612d334d7
Albert Stjärne <albert.stjarne@gmail.com>
2019-04-23 16:52:15 +0200

    adds methods nextSlide and prevSlide

---
+++ main.js
@@ -0,0 +1,38 @@
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('#next');
+const prev = document.querySelector('#prev');
+const auto = false;
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+  // Get current class
+  const current = document.querySelector('.current');
+  // remove current class
+  current.classList.remove('current');
+  // Check for next slide
+  if(current.nextElementSibling) {
+    // Add current to next sibling
+    current.nextElementSibling.classList.add('current');
+  } else {
+    // Add current to start
+    slides[0].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
+}
+
+const prevSlide = () => {
+  // Get current class
+  const current = document.querySelector('.current');
+  // remove current class
+  current.classList.remove('current');
+  // Check for prev slide
+  if(current.previousElementSibling) {
+    // Add current to next sibling
+    current.previousElementSibling.classList.add('current');
+  } else {
+    // Add current to last
+    slides[slides.length - 1].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
+}
